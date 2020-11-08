a34b7a72a4661e192784ab9f9ef0ef1ca2151898
Lucas Merchant <lbmerchant93@gmail.com>
2020-11-07 18:10:23 -0700

    Work on keeping DOM and data model seperate

--- game.js
+++ game.js
@@ -2,50 +2,45 @@ class Game {
   constructor() {
     this.player1 = new Player('One', 'X');
     this.player2 = new Player('Two', 'O');
-    this.currentPlayer = this.player1;
+    this.currentPlayer;
+    this.waitingPlayer;
+    this.gamesPlayed = 0;
+    if (this.gamesPlayed % 2 === 0) {
+      this.currentPlayer = this.player1;
+    } else if (this.gamesPlayed % 2 != 0) {
+      this.currentPlayer = this.player2;
+    }
     this.boxesFilled = 0;
   };
-  decidePlayerTurn() {
+  claimBox() {
     this.boxesFilled += 1;
+  }
+  decidePlayerTurn() {
     if (this.currentPlayer.token === 'X') {
       this.currentPlayer = this.player2;
+      this.waitingPlayer = this.player1;
     } else {
       this.currentPlayer = this.player1;
+      this.waitingPlayer = this.player2;
     };
   };
+  chooseWinner() {
+    this.waitingPlayer.winCount += 1;
+  }
   checkForWinner() {
-    if (boxOne.innerText === boxTwo.innerText && boxOne.innerText === boxThree.innerText) {
-      messageDisplay.innerText = `${boxOne.innerText} won!`;
-    } else if (boxFour.innerText === boxFive.innerText && boxFour.innerText === boxSix.innerText) {
-      messageDisplay.innerText = `${boxFour.innerText} won!`;
-    } else if (boxSeven.innerText === boxEight.innerText && boxSeven.innerText === boxNine.innerText) {
-      messageDisplay.innerText = `${boxSeven.innerText} won!`;
-    } else if (boxOne.innerText === boxFour.innerText && boxOne.innerText === boxSeven.innerText) {
-      messageDisplay.innerText = `${boxOne.innerText} won!`;
-    } else if (boxTwo.innerText === boxFive.innerText && boxTwo.innerText === boxEight.innerText) {
-      messageDisplay.innerText = `${boxTwo.innerText} won!`;
-    } else if (boxThree.innerText === boxSix.innerText && boxThree.innerText === boxNine.innerText) {
-      messageDisplay.innerText = `${boxThree.innerText} won!`;
-    } else if (boxOne.innerText === boxFive.innerText && boxOne.innerText === boxNine.innerText) {
-      messageDisplay.innerText = `${boxOne.innerText} won!`;
-    } else if (boxThree.innerText === boxFive.innerText && boxThree.innerText === boxSeven.innerText) {
-      messageDisplay.innerText = `${boxThree.innerText} won!`;
-    }
-    if (messageDisplay.innerText.includes('won!')) {
-      for (var i = 0; i < boxes.length; i++) {
-        boxes[i].classList.add('disable');
-      };
-    };
+    checkWinningConditions();
+    disableBoxesAfterWin();
   };
   checkForDraw() {
-    if(!messageDisplay.innerText.includes('won!')) {
-      messageDisplay.innerText = `Its a draw!`
-    }
+    checkDrawConditions();
   };
   saveWinningBoard() {
 
   };
   resetGame() {
-
+    this.gamesPlayed += 1;
+    this.boxesFilled = 0;
+    var timer;
+    timer = setTimeout(createNewGame, 1000);
   };
 };
--- main.js
+++ main.js
@@ -11,7 +11,7 @@ var boxEight = document.querySelector('.eight');
 var boxNine = document.querySelector('.nine');
 
 // global variables
-var game = new Game();
+var currentGame = new Game();
 var boxes = [boxOne, boxTwo, boxThree, boxFour, boxFive, boxSix, boxSeven, boxEight, boxNine];
 
 //eventListeners
@@ -29,26 +29,77 @@ boxNine.addEventListener('click', displayPlayerInput);
 function displayPlayerInput(event) {
   for (var i = 0; i < boxes.length; i++) {
     if (event.target.id === boxes[i].id) {
-      boxes[i].innerText = `${game.currentPlayer.token}`;
+      boxes[i].innerText = `${currentGame.currentPlayer.token}`;
       boxes[i].classList.add('disable');
       boxes.splice(i, 1);
     };
   };
-  game.decidePlayerTurn();
-  messageDisplay.innerText = `It's ${game.currentPlayer.token}'s turn`;
-  if (game.boxesFilled >= 5 && game.boxesFilled <= 8) {
-    game.checkForWinner();
-  } else if (game.boxesFilled === 9){
-    game.checkForDraw();
+  currentGame.claimBox();
+  currentGame.decidePlayerTurn();
+  messageDisplay.innerText = `It's ${currentGame.currentPlayer.token}'s turn`;
+  if (currentGame.boxesFilled >= 5 && currentGame.boxesFilled <= 9) {
+    currentGame.checkForWinner();
+    if (messageDisplay.innerText.includes('won!')) {
+      currentGame.chooseWinner();
+      currentGame.resetGame();
+    };
   }
+  if (currentGame.boxesFilled === 9) {
+    currentGame.checkForDraw();
+    currentGame.resetGame();
+  };
 };
 
+function checkWinningConditions() {
+  if (boxOne.innerText === boxTwo.innerText && boxTwo.innerText === boxThree.innerText && boxOne.innerText != '') {
+    messageDisplay.innerText = `${boxOne.innerText} won!`;
+  } else if (boxFour.innerText === boxFive.innerText && boxFive.innerText === boxSix.innerText && boxFour.innerText != '') {
+    messageDisplay.innerText = `${boxFour.innerText} won!`;
+  } else if (boxSeven.innerText === boxEight.innerText && boxEight.innerText === boxNine.innerText && boxSeven.innerText != '') {
+    messageDisplay.innerText = `${boxSeven.innerText} won!`;
+  } else if (boxOne.innerText === boxFour.innerText && boxFour.innerText === boxSeven.innerText && boxOne.innerText != '') {
+    messageDisplay.innerText = `${boxOne.innerText} won!`;
+  } else if (boxTwo.innerText === boxFive.innerText && boxFive.innerText === boxEight.innerText && boxTwo.innerText != '') {
+    messageDisplay.innerText = `${boxTwo.innerText} won!`;
+  } else if (boxThree.innerText === boxSix.innerText && boxSix.innerText === boxNine.innerText && boxThree.innerText != '') {
+    messageDisplay.innerText = `${boxThree.innerText} won!`;
+  } else if (boxOne.innerText === boxFive.innerText && boxFive.innerText === boxNine.innerText && boxOne.innerText != '') {
+    messageDisplay.innerText = `${boxOne.innerText} won!`;
+  } else if (boxThree.innerText === boxFive.innerText && boxFive.innerText === boxSeven.innerText && boxThree.innerText != '') {
+    messageDisplay.innerText = `${boxThree.innerText} won!`;
+  };
+};
 
+function declareWinner() {
+  if (messageDisplay.innerText.includes('won!')) {
+    game.chooseWinner();
+  };
+};
 
+function disableBoxesAfterWin() {
+  if (messageDisplay.innerText.includes('won!')) {
+    for (var i = 0; i < boxes.length; i++) {
+      boxes[i].classList.add('disable');
+    };
+  };
+};
 
+function checkDrawConditions() {
+  if (!messageDisplay.innerText.includes('won!')) {
+    messageDisplay.innerText = `Its a draw!`
+  };
+};
 
+function createNewGame() {
+  boxes = [boxOne, boxTwo, boxThree, boxFour, boxFive, boxSix, boxSeven, boxEight, boxNine];
+  for (var i = 0; i < boxes.length; i++) {
+    boxes[i].classList.remove('disable');
+    boxes[i].innerText = ``;
+  };
+  messageDisplay.innerText = `It's ${currentGame.currentPlayer.token}'s turn`;
+};
 
+function displayWinCount() {
 
-
-
+}
 //
